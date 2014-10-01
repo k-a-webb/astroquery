2e0e9df581c5967a09b53884039dbaa4f3d57564
mtbannister <michele@mso.anu.edu.au>
2014-09-30 18:29:10 -0700

    Test experiments with returning specific subsets of a and orbit type.

--- astroquery/mpc/tests/test_tno_groups.py
+++ astroquery/mpc/tests/test_tno_groups.py
@@ -8,11 +8,15 @@ from astroquery import mpc
 test_instance = mpc.Mpc()
 test_instance._login('mpc_ws', 'mpc!!ws')
 
-# tnos = 'semimajor_axis_min 15'
-tnos = 'orbit_type 14'
-print(len(test_instance.query_parameters(tnos, return_request='semimajor_axis')))
+# Test orbit type cycling
+orbit_types = range(0, 11)
+orbit_types += [14, 15, 16]
 
+for orbtype in orbit_types[12:13]:
+    print(len(test_instance.query_parameters('orbit_type '+ str(orbtype), return_request='inclination')))
 
+# Test specific condition setting: all TNOs
+print(len(test_instance.query_parameters('semimajor_axis_min 15', return_request='semimajor_axis, inclination, eccentricity')))
 
 # class MyTestCase(unittest.TestCase):
 #     def test_something(self):
